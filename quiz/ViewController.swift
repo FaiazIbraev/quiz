01a5b969332f16f64bb10cc8cae9bec1b82f6d07
FaiazIbraev <fayaz.ibraev@gmail.com>
2022-06-25 12:41:21 +0600

    Initial Commit

---
+++ quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quiz
+//
+//  Created by Faiaz Ibraev on 25/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
